7eacb8a8b321613974bf89df51c3924e72bef389
mohammadumar28 <mohammadumar28@gmail.com>
2020-09-11 20:25:16 +0530

    Add update, edit, show methods and routes in the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,42 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
