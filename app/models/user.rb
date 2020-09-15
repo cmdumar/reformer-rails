b585a5317a0773dc4e77aa41c580d3694afd0266
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-09-15 05:21:02 -0300

    Add form partial to users view. Remove minimum length validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { in: 5..20 }
+  validates :username, presence: true, length: { maximum: 20 }
   validates :password, presence: true, length: { minimum: 8 }
-  validates :email, presence: true, length: { in: 10..30 }
+  validates :email, presence: true, length: { maximum: 30 }
 end
